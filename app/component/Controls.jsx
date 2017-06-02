a67967faad59bbf530d977bdd4a33dd9bafe82f3
Geoff Simons <geoff.simons@gmail.com>
2017-06-01 20:06:45 -0700

    connected redux to wheel.angle

--- app/component/Controls.jsx
+++ app/component/Controls.jsx
@@ -7,18 +7,25 @@ function Controls(props) {
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const form = evt.target;
-    debug('FORM:', form);
+    // debug('FORM:', form);
     debug('Angle:', form.angle.value);
+    props.updateWheel(Number(form.angle.value));
   }
   return(
     <div className="controls">
       <form onSubmit={handleSubmit}>
-        <input name="angle" />
+        <div>
+          <input name="angle" autoComplete="off" />
+        </div>
         <button type="submit">Update</button>
       </form>
     </div>
   );
 }
 // <form onSubmit={handleSubmit}>
+
+Controls.propTypes = {
+  updateWheel: PropTypes.func
+}
 
 export default Controls;
--- app/component/index.jsx
+++ app/component/index.jsx
@@ -1,16 +1,32 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Wheel from './wheel';
 import Controls from './Controls';
+import { updateWheel } from '../action';
 
 class App extends Component {
+
   render() {
+    const { dispatch, angle } = this.props;
     return(
       <div>
-        <Wheel angle={45} />
-        <Controls />
+        <Wheel angle={angle} />
+        <Controls updateWheel={ang => dispatch(updateWheel(ang))}/>
       </div>
     )
   }
 }
 
-export default App;
+App.propTypes = {
+  angle: PropTypes.number
+};
+
+function mapStateToProps(state) {
+  const { wheel } = state;
+  return {
+    angle: wheel.angle
+  };
+}
+
+export default connect(mapStateToProps)(App);
