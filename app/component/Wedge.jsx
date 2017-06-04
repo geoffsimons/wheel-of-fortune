ecbe3225ab54b711a5c6873cdf32f9edbe06f6ad
Geoff Simons <geoff.simons@gmail.com>
2017-06-03 17:45:14 -0700

    Fixed tan() infinity bug, added static prize array

--- app/component/Wedge.jsx
+++ app/component/Wedge.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import debug from '../util/debug';
+// const debug = console.log;
 
 function Wedge(props) {
   const { angle, label, bgcolor, color, offset } = props;
-  const h = Math.floor(100 * Math.tan(angle * Math.PI / 180));
-  const points = `100,100 200,${100-h} 200,${100+h}`;
+  debug('ANGLE:', angle);
+  const r = 100; // TODO: Use props to get r.
+  const cx = r;
+  const cy = r;
+  const p = {
+    x: cx + r * Math.cos(angle * Math.PI / 180),
+    y: cy + r * Math.sin(angle * Math.PI / 180)
+  };
+  // const h = Math.min(100, Math.floor(100 * Math.tan(angle * Math.PI / 90)));
+  // const points = `100,100 200,${100-h} 200,${100+h}`;
+  const points = `${cx},${cy} ${p.x},${p.y} ${cx+r},${cy+r} ${cx+r},${cy}`;
   const rot = `rotate(${offset}, 100, 100)`;
   const style = {
     position: 'absolute',
     top: 0,
-    left: 0
+    left: 0,
+    transform: `rotate(${offset}deg)`,
+    transformOrigin: '100px 100px',
+    // border: '1px solid red'
   };
-
+  debug('POINTS:', points);
+  debug('ROT:', rot);
   return(
     <svg width="200" height="200"
       style={style}
@@ -22,14 +37,16 @@ function Wedge(props) {
         </clipPath>
       </defs>
       <circle fill={bgcolor} cx="100" cy="100" r="100"
-              clipPath="url(#myClip)"
-              transform={rot} />
+              clipPath="url(#myClip)"/>
       <text x="195" y="113" textAnchor="end"
-              fontFamily="Arial" fontSize="35" stroke="#000" fill={color || '#000'}
-              transform={rot}>{label}</text>
+              fontFamily="Arial" fontSize="35"
+              stroke="#000" fill={color || '#000'}
+              transform={`rotate(${angle/2}, ${cx},${cy})`}>{label}</text>
     </svg>
   )
 }
+// <polygon points={points} />
+
 
 Wedge.propTypes = {
   color: PropTypes.string,
--- app/component/Wheel.jsx
+++ app/component/Wheel.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import debug from '../util/debug';
+// const debug = console.log;
 import Wedge from './Wedge';
 
 // TODO: Build wedges from prizes array.
 // TODO: Accept a prop for radius, or for width & height.
 function Wheel(props) {
+  const prizes = [
+    {
+      color: '#975bc1',
+      weight: 1,
+      label: '100'
+    },
+    {
+      color: '#b23caa',
+      weight: 1,
+      label: '50'
+    },
+    {
+      color: '#41bf73',
+      weight: 1,
+      label: '250'
+    },
+    {
+      color: '#b23caa',
+      weight: 1,
+      label: '50'
+    },
+    {
+      color: '#443aad',
+      weight: 1,
+      label: '150'
+    },
+    {
+      color: '#975bc1',
+      weight: 1,
+      label: '100'
+    },
+    {
+      color: '#b23caa',
+      weight: 1,
+      label: '50'
+    },
+    {
+      color: '#e5e059',
+      weight: 1,
+      label: '200'
+    },
+  ];
   const style = {
     transform: `rotate(${props.angle}deg)`,
     transformOrigin: '100px 100px',
@@ -15,18 +58,33 @@ function Wheel(props) {
     width: '200px' // Prevents a weird z-index overlap
   };
   // debug('TRANS:', trans);
-  const h = Math.floor(100 * Math.tan(30 * Math.PI / 180));
-  const points = `100,100 200,${100-h} 200,${100+h}`;
+  let sum = 0;
+  prizes.forEach(p => sum += p.weight);
+  const base = 360 / sum;
+  debug('SUM:', sum);
+  debug('BASE:', base);
+  let offset = 0;
+  prizes.forEach(p => {
+    p.offset = offset;
+    p.angle = base * p.weight;
+    offset += p.angle;
+  });
+  const wedges = prizes.map((p, index) => {
+    return <Wedge key={index} bgcolor={p.color} label={p.label} angle={p.angle} offset={p.offset} />;
+  });
+  debug('PRIZES:', prizes);
+  debug('WEDGES:', wedges);
   return(
      <div id="wheel" style={style}>
-       <Wedge bgcolor="#975bc1" label="100" angle={30} offset={0} />
-       <Wedge bgcolor="#b23caa" label="50"  angle={30} offset={72} />
-       <Wedge bgcolor="#41bf73" label="150" angle={30} offset={144} />
-       <Wedge bgcolor="#443aad" label="200" angle={30} offset={216} />
-       <Wedge bgcolor="#e5e059" label="250" angle={30} offset={288} />
+       {wedges}
      </div>
   );
 }
+// <Wedge bgcolor="#975bc1" label="100" angle={30} offset={0} />
+// <Wedge bgcolor="#b23caa" label="50"  angle={30} offset={72} />
+// <Wedge bgcolor="#41bf73" label="150" angle={30} offset={144} />
+// <Wedge bgcolor="#443aad" label="200" angle={30} offset={216} />
+// <Wedge bgcolor="#e5e059" label="250" angle={30} offset={288} />
 
 Wheel.propTypes = {
   prizes: PropTypes.array,
